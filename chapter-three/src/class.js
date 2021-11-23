6f927aa3b529a2be0c978bf8b775682e72765a2b
Lizhen <lzhuang@thoughtworks.com>
2021-11-23 12:03:23 +0800

    finish homework

--- chapter-three/src/class.js
+++ chapter-three/src/class.js
@@ -1,4 +1,42 @@
 class Class {
+    constructor(number) {
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    registerAssignLeaderListener(teacher) {
+        this.teacher = teacher;
+    }
+
+    registerJoinListener(teacher) {
+        this.teacher = teacher;
+    }
+
+    assignLeader(student) {
+        if (this.isIn(student)) {
+            this.leader = student;
+            if (this.teacher) {
+                console.log("I am " + this.teacher.name + ". I know " + student.name + " become Leader of Class " + this.number + ".");
+            }
+        } else {
+            this.leader = null;
+            console.log("It is not one of us.");
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        if (this.teacher && this.teacher.isTeaching(student)) {
+            console.log("I am " + this.teacher.name + ". I know " + student.name + " has joined Class " + student.klass.number + ".")
+        }
+    }
+
+    isIn(student) {
+        return student.klass.number === this.number;
+    }
 
 }
 
--- chapter-three/src/teacher.js
+++ chapter-three/src/teacher.js
@@ -1,7 +1,25 @@
 import Person from './person';
 
-class Teacher extends Person{
+class Teacher extends Person {
+    constructor(id, name, age, klasses = []) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
 
+    introduce() {
+        let information = super.introduce() + ` I am a Teacher.`;
+        if (this.klasses.length === 0) {
+            information += ` I teach No Class.`;
+        } else {
+            information += ` I teach Class `;
+            this.klasses.forEach((klass, index) => (index === (this.klasses.length - 1) ? information += `${klass.number}.` : information += `${klass.number}, `))
+        }
+        return information;
+    }
+
+    isTeaching(student) {
+        return this.klasses.some(klass => klass.isIn(student));
+    }
 }
 
 export default Teacher;
